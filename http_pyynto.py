54b2f29ef16b568562717664fe6c19d01c032aa6
Miicca <105639463+Miicca@users.noreply.github.com>
2022-09-19 15:07:34 +0300

    Toka commit

--- http_pyynto.py
+++ http_pyynto.py
@@ -2,7 +2,11 @@ import urllib.request
 import json
 
 
-def hae_postinumerot():
+def hae_postinumerot() -> dict:
+    '''
+    Tekee HTTP-pyynnön GitHubissa sijaitsevaan JSON-tiedostoon ja palauttaa Python-sanakirjan,
+    jossa avaimina on postinumeroita, ja arvoina postitoimipaikkojen nimiä.
+    '''
     with urllib.request.urlopen('https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json') as response:
         data = response.read()
 
